7a666d22463077180cef5060869c4590ffabaabc
fatfrog9 <Lukas.Birkemeyer@gmail.com>
2022-09-08 16:18:07 +0200

    first tests

--- main.py
+++ main.py
@@ -17,19 +17,22 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    # meta parameters
-    dimension = 3
-    resolution = 32 # in bites
-    x = 51
-    y = 23
-    z = 235
+    resolution = 4  # anzahl der bits, die nötig sind um die werte im originalen array abzubilden (z.B. 4 für werte zwischen 0-15)
 
+    # meta parameters
+    np_array = np.array([[15, 15], [3, 4]])
+    morton_codes = []
+
+    value_cnt, dimension = np_array.shape
 
     m = morton.Morton(dimensions=dimension, bits=resolution)
-    code = m.pack(x, y, z)  # pack two values
 
-    values = m.unpack(code)  # should get back 13 and 42
+    for i in range(0, value_cnt):
+        morton_codes.append(m.pack(np_array[i, 0], np_array[i, 1])) #hier ggf. noch die Dimension anpassen
 
-    print(values)
+    for code in morton_codes:
+        print(code)
+        print(m.unpack(code))
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
