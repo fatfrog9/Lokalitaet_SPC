85cf568711e9dcb2adf412bacf6158f86670734f
fatfrog9 <Lukas.Birkemeyer@gmail.com>
2022-09-12 15:45:30 +0200

    add data and hilbert curve

--- main.py
+++ main.py
@@ -12,42 +12,43 @@ import morton
 from tqdm import tqdm
 import sys
 from scipy.spatial import distance
-from multiprocessing import Pool
+from hilbertcurve.hilbertcurve import HilbertCurve
 from decimal import *
 
 
 dim3 = False
 
 
-def generateArray_df(resolution, dimension):
+def generateArray_df_morton(resolution, dimension):
     max_value = (2 ** resolution)
     dim = 2 if dim3 == False else 3
     m = morton.Morton(dimensions=dimension, bits=resolution)
+    hilbert_curve = HilbertCurve(resolution, dimension, n_procs=0)
 
-    df_array = pd.DataFrame(columns=['x', 'y', 'morton'])
+    df_array = pd.DataFrame(columns=['x', 'y', 'morton', 'hilbert'])
 
     for cnt_one in tqdm(range (0, max_value)):
         for cnt_two in range(0, max_value):
-            temp_df = pd.DataFrame([{'x': cnt_one, 'y': cnt_two, 'morton': m.pack(cnt_one, cnt_two)}])
+            temp_df = pd.DataFrame([{'x': cnt_one, 'y': cnt_two, 'morton': m.pack(cnt_one, cnt_two), 'hilbert' : hilbert_curve.distance_from_point((cnt_one, cnt_two))}])
+            # print(temp_df)
             df_array = pd.concat([df_array, temp_df], axis=0, ignore_index=True)
 
-    return df_array, m
+    return df_array, m, hilbert_curve
 
 
-def plotScatterAnnotationLatentSpace_df(df_array, m):
+def plotScatterAnnotationLatentSpace_df(df_array, curve):
 
     df_array.plot(x = 'x', y = 'y', marker="o")
     for idx, row in df_array.iterrows():
-        plt.annotate(row['morton'], (row['x']+0.02, row['y']+0.02))
+        plt.annotate(row[curve], (row['x']+0.02, row['y']+0.02))
 
-    df_array = df_array.sort_values(by='morton')
+    df_array = df_array.sort_values(by=curve)
     # print(df_array)
     df_array.reset_index()
 
     df_array.plot(x='x', y='y', marker="o")
     for idx, row in df_array.iterrows():
-        plt.annotate(row['morton'], (row['x'] + 0.02, row['y'] + 0.02))
-    plt.show()
+        plt.annotate(row[curve], (row['x'] + 0.02, row['y'] + 0.02))
 
 
 def calcMaximumDistanceBetweenPoints(np_array_morton):
@@ -73,7 +74,7 @@ def calcMaximumDistanceBetweenPoints(np_array_morton):
           max_dist_B)
 
 
-def determineSampleRateExperimental(df_array, rangeThreshold):
+def determineSampleRateExperimental(df_array, rangeThreshold, curve):
 
     print("Determine Sample Rate.")
     maxMortonDist = 0
@@ -81,7 +82,7 @@ def determineSampleRateExperimental(df_array, rangeThreshold):
     distMaxPoint = (0,0)
 
     for j in tqdm(range(0, len(df_array))):
-        distInRange_df = pd.DataFrame(columns=['x', 'y', 'morton', 'dist'])
+        distInRange_df = pd.DataFrame(columns=['x', 'y', curve, 'dist'])
         refPoint = (df_array['x'][j], df_array['y'][j])
 
         for i in range(0, len(df_array)):
@@ -92,15 +93,15 @@ def determineSampleRateExperimental(df_array, rangeThreshold):
             rangeThreshold_temp = int(rangeThreshold*100)
 
             if dist_temp < rangeThreshold_temp:
-                temp_df = pd.DataFrame([{'x' : df_array['x'][i] , 'y' : df_array['y'][i], 'morton' : df_array['morton'][i], 'dist' : dist}])
+                temp_df = pd.DataFrame([{'x' : df_array['x'][i] , 'y' : df_array['y'][i], curve : df_array[curve][i], 'dist' : dist}])
                 distInRange_df = pd.concat([distInRange_df, temp_df], axis=0, ignore_index=True)
 
         #print(distInRange_df)
         #print("max:", distInRange_df['morton'].max(), "min:", distInRange_df['morton'].min())
 
-        mortonRefPoint = df_array['morton'][j]
-        mortonMin = distInRange_df['morton'].min()
-        mortonMax = distInRange_df['morton'].max()
+        mortonRefPoint = df_array[curve][j]
+        mortonMin = distInRange_df[curve].min()
+        mortonMax = distInRange_df[curve].max()
 
         mortonMin = mortonRefPoint - mortonMin if mortonMin < mortonRefPoint else mortonMin - mortonRefPoint
         mortonMax = mortonMax - mortonRefPoint if mortonMax > mortonRefPoint else mortonRefPoint - mortonMax
@@ -114,16 +115,16 @@ def determineSampleRateExperimental(df_array, rangeThreshold):
             refMaxPoint = (df_array['x'][j], df_array['y'][j])
 
             if mortonMax > mortonMin:
-                distMaxPoint = (distInRange_df['x'][distInRange_df['morton'].astype(float).idxmax()],
-                        distInRange_df['y'][distInRange_df['morton'].astype(float).idxmax()])
+                distMaxPoint = (distInRange_df['x'][distInRange_df[curve].astype(float).idxmax()],
+                        distInRange_df['y'][distInRange_df[curve].astype(float).idxmax()])
             else:
-                distMaxPoint = (distInRange_df['x'][distInRange_df['morton'].astype(float).idxmin()],
-                        distInRange_df['y'][distInRange_df['morton'].astype(float).idxmin()])
+                distMaxPoint = (distInRange_df['x'][distInRange_df[curve].astype(float).idxmin()],
+                        distInRange_df['y'][distInRange_df[curve].astype(float).idxmin()])
 
 
         # print("Point: ", curPoint, "Dist: ", curDist)
         #print("MaxMortonDist_current:", curMaxMortonDist)
-    print("The maximum Morton Distance is experimentally determined as ", maxMortonDist, " between P_ref", refMaxPoint, "and P_min=", distMaxPoint)
+    print("The maximum", curve, " Distance is experimentally determined as ", maxMortonDist, " between P_ref", refMaxPoint, "and P_min=", distMaxPoint)
     return maxMortonDist, refMaxPoint, distMaxPoint
 
 
@@ -165,21 +166,30 @@ if __name__ == '__main__':
             print("Let's determine the (half) Sample Rate in latent space;"
                     "maximum distance between points that have an euclidean distance of max: ", rangeThreshold)
 
-            for resolution in range(2,12):
+            for resolution in range(7,8):
 
                 print("The resolution is set to", resolution, "Bits.")
 
                 print("Generate array.")
-                df_array, m = generateArray_df(resolution=resolution, dimension=2)
+                df_array, m, hilbert_curve = generateArray_df_morton(resolution=resolution, dimension=2)
 
-                determineSampleRateExperimental(df_array, rangeThreshold)
+                # print(df_array)
+
+                #determineSampleRateExperimental(df_array, rangeThreshold, 'morton')
+                determineSampleRateExperimental(df_array, rangeThreshold, 'hilbert')
                 calculateSampleRate(resolution, m)
 
                 print()
                 # print("Determine maximum distance of datapoint with a resolution of", resolution, "Bits.")
                 # calcMaximumDistanceBetweenPoints(np_array_morton)
 
+                plotScatterAnnotationLatentSpace_df(df_array, 'morton')
+                plotScatterAnnotationLatentSpace_df(df_array, 'hilbert')
+                # df_array.rename(columns={'x': 'y',
+                #            'y': 'x'}, inplace=True)
                 # plotScatterAnnotationLatentSpace_df(df_array, m)
+
+                plt.show()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
